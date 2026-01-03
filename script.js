c2d5d4baa86c26dcda6c3261108412123f465598
Rahul Kr <rahulkumar8604745@gmail.com>
2026-01-03 23:17:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // your code here
 const btn = document.querySelector("#button")
-const h3 = document.
-btn.addEventLisneter("click",() => {
-	
+const h3 = document.querySelector("#url")
+btn.addEventListener("click",(e) => {
+e.preventDefault()
+	h3.innerText = "https://d3dyfaf3iutrxo.cloudfront.net/thumbnail/assignment/question/8bb3b47db47841178b670717ea3c74f9.png"
 })
