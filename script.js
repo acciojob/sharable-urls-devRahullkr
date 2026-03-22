0447ba080206c3415e5ce73fe1161cfc5bd1d617
Rahul Kr <rahulkumar8604745@gmail.com>
2026-03-22 22:48:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const btn = document.querySelector("#button")
 const name = document.querySelector("#name")
 const year = document.querySelector("#year")
 const h3 = document.querySelector("#url")
-// btn.addEventListener("click",(e) => {
-// e.preventDefault()
-// 	let n = name.value
-// 	let y = year.value
-// 	h3.innerText = `https://localhost:8080/?name=${n}&year=${y}`
-// })
+btn.addEventListener("click",(e) => {
+e.preventDefault()
+	let n = name.value
+	let y = year.value
+	h3.innerText = `https://localhost:8080/?name=${n}&year=${y}`
+})
